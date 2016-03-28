d91f6902a2e75f68ad68d84c2b7e8ac91d6dfaed
yoooda <yodasatla@gmail.com>
2016-03-28 10:48:50 +0300

    fixed get Ip

--- modules/getIP.py
+++ modules/getIP.py
@@ -1,6 +1,6 @@
 import socket
 def run(**args):
-    ip = socket.gethosybyname(socket.gethostname())
+    ip = socket.gethostbyname(socket.gethostname())
     if (ip != "127.0.0.1" and ip != None):
         print "[*] Obtained IP address %s" %ip
         return str(ip)
